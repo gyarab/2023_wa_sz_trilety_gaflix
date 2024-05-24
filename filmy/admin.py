d37e48ca0cfe35916f67298504277378c200c24b
Radim Tříletý <radim.trilety@student.gyarab.cz>
2024-05-24 09:19:49 +0200

    test

--- filmy/admin.py
+++ filmy/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from filmy.models import Movie, Director, Genre
+from filmy.models import Movie, Director, Genre, Actor
 
 # Register your models here.
 class MovieAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id' , 'name', 'year']
 
 class DirectorAdmin(admin.ModelAdmin):
     pass
@@ -11,8 +11,14 @@ class DirectorAdmin(admin.ModelAdmin):
 class GenreAdmin(admin.ModelAdmin):
     pass
 
+class ActorAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Movie, MovieAdmin)
 
 admin.site.register(Director, DirectorAdmin)
 
 admin.site.register(Genre, GenreAdmin)
+
+admin.site.register(Actor, ActorAdmin)
+
