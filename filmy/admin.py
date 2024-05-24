bd78c5f4cba2b0f8bd7d0170a3df24cc62aa79ef
Radim Tříletý <radim.trilety@student.gyarab.cz>
2024-05-24 09:27:35 +0200

    test

--- filmy/admin.py
+++ filmy/admin.py
@@ -3,7 +3,10 @@ from filmy.models import Movie, Director, Genre, Actor
 
 # Register your models here.
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['id' , 'name', 'year']
+    list_display = ['id' , 'name', 'year', 'genres_display', 'director']
+    list_display_links = ['id', 'name']
+    search_fields = ['name', 'director_mame']
+    list_filter = ['genres' , 'year']
 
 class DirectorAdmin(admin.ModelAdmin):
     pass
