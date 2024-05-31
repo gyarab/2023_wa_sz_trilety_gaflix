b380cafa6319ee6f87625852fcebdff9774a49a6
Radim Tříletý <radim.trilety@student.gyarab.cz>
2024-05-31 08:24:34 +0200

    test

--- filmy/models.py
+++ filmy/models.py
@@ -5,7 +5,7 @@ class Movie(models.Model):
     year = models.PositiveSmallIntegerField(blank=True, null=True)
     footage = models.PositiveSmallIntegerField(blank=True, null=True, help_text="in minutes")
     description = models.TextField(blank=True)
-    main_picture = models.CharField(blank=True, default="", max_length=2000)
+    main_picture = models.CharField(blank=True, default="", max_length=2048)
 
     director = models.ForeignKey('Director', blank = True, null = True, on_delete = models.SET_NULL)
     actors = models.ManyToManyField('Actor', blank = True)
@@ -23,7 +23,7 @@ class Director(models.Model):
     name = models.CharField(max_length=300)
     birth_year = models.PositiveSmallIntegerField(blank=True, null=True)
     description = models.TextField(blank=True)
-    main_picture = models.CharField(blank=True, default="", max_length=2000)
+    main_picture = models.CharField(blank=True, default="", max_length=2048)
 
 
     def __str__(self):
@@ -33,7 +33,7 @@ class Actor(models.Model):
     name = models.CharField(max_length=300)
     birth_year = models.PositiveSmallIntegerField(blank=True, null=True)
     description = models.TextField(blank=True)
-    main_picture = models.CharField(blank=True, default="", max_length=2000)
+    main_picture = models.CharField(blank=True, default="", max_length=2048)
 
 
     def __str__(self):
